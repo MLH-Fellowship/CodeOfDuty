9e837c39f82062bd43059d8b138242c59e273888
vrushti-mody <vrushtimody6@gmail.com>
2020-11-19 03:20:50 +0530

    Added fetch sprint route

--- codeofduty-backend/__tests__/routes.test.js
+++ codeofduty-backend/__tests__/routes.test.js
@@ -5,3 +5,10 @@ beforeAll(async () => {
     const uri = require('./config').ATLAS_URI+`${databaseName}`
   await mongoose.connect(url, { useNewUrlParser: true })
 })
+
+it('Should return sprints of a user', async done => {
+    // Sends request...
+    const res = await request.get('/fetchSprints/vrushti-mody')
+    expect(res).toBeTruthy()
+    done()
+  })
