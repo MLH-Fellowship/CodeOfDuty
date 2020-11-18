60580827d18d409935bc61fe2c476811107c21e1
Ajwad Shaikh <shaikhajwad10@gmail.com>
2020-11-18 20:44:19 +0530

    Added Mongoose Schema and Model

--- codeofduty-backend/app.js
+++ codeofduty-backend/app.js
@@ -1,25 +1,60 @@
 //Libraries
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
+const models = require("./models");
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Server listening on port
 const port = process.env.port || 5000;
 const server = app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+  console.log(`Server running on port ${port}`);
+});
 
 // Uri to connect mongodb
-const uri = require('./config').ATLAS_URI;
+const uri = require("./config").ATLAS_URI;
 
 // Connect to DB
-mongoose.connect(uri, {useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true});
-mongoose.connection.once('open', () => {
-    console.log('Connected to MongoDB');
-})
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  const sampleRepo = new models.Repo({
+    _id: "owner/repo",
+    repo_url: "https://codeofduty.com/owner/repo",
+    maintainers: ["cqvu", "ajwad-shaikh", "vrusti-mody"],
+    past_sprints: [
+      {
+        sprint_object: "542c2b97bac0595474108b48",
+        sprint_name: "Sprint 5 | v1.2.2",
+        sprint_url: "https://codeofduty.com/owner/repo/10",
+      },
+    ],
+    active_sprints: [
+      {
+        sprint_object: "542c2b97bac0595474108b48",
+        sprint_name: "Sprint 6 | v1.3.2",
+        sprint_url: "https://codeofduty.com/owner/repo/12",
+      },
+    ],
+    contributors: [
+      {
+        user: "cqvu",
+        points_claimed: 20,
+      },
+      {
+        user: "ajwad-shaikh",
+        points_claimed: 10,
+      },
+    ],
+  });
+  console.log(sampleRepo);
+});
---
+++ codeofduty-backend/models.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+const sprintOverviewSchema = new mongoose.Schema({
+  sprint_object: String,
+  sprint_name: String,
+  sprint_url: String,
+});
+
+const repoLevelContributorSchema = new mongoose.Schema({
+  user: String,
+  points_claimed: Number,
+});
+
+const sprintLevelContributorSchema = new mongoose.Schema({
+  user: String,
+  points_claimed: Number,
+  points_at_stake: Number,
+});
+
+const taskSchema = new mongoose.Schema({
+  issue_url: String,
+  pr_url: String,
+  task_status: String,
+  contributor: String,
+  reviewer: String,
+  contributor_points: Number,
+  reviewer_points: Number,
+});
+
+const repoSchema = new mongoose.Schema({
+  _id: String,
+  repo_url: String,
+  maintainers: [String],
+  past_sprints: [sprintOverviewSchema],
+  active_sprints: [sprintOverviewSchema],
+  contributors: [repoLevelContributorSchema],
+});
+
+const sprintSchema = new mongoose.Schema({
+  repo: String,
+  name: String,
+  sprint_url: String,
+  milestone_url: String,
+  contributors: [sprintLevelContributorSchema],
+  tasks: [taskSchema],
+  boss_health: Number,
+  victory_threshold: Number,
+});
+
+const Repo = mongoose.model("Repo", repoSchema);
+
+const Sprint = mongoose.model("Sprint", sprintSchema);
+
+module.exports = {
+  Repo,
+  Sprint,
+};
