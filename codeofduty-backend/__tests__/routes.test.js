36b8b553d328db9a1d0d9926b21d54e0a1487b06
Ajwad Shaikh <shaikhajwad10@gmail.com>
2020-11-19 20:16:32 +0530

    Fix test leakage

--- codeofduty-backend/__tests__/routes.test.js
+++ codeofduty-backend/__tests__/routes.test.js
@@ -3,24 +3,31 @@ require("dotenv").config();
 const request = require("supertest");
 const app = require("../app");
 
-beforeAll(async () => {
+beforeAll(async (done) => {
   const uri = process.env.ATLAS_URI;
-  await mongoose
-    .connect(uri, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-    });
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  });
+  done();
 });
 
-it("Should return sprints of a user", async () => {
+it("Should return sprints of a user", async (done) => {
   // Sends request...
   const user = await request(app).get("/fetchSprints?user=vrushti-mody");
   expect(user).toBeTruthy();
+  done();
 });
 
-it("Should return all sprints (Top 10)", async () => {
+it("Should return all sprints (Top 10)", async (done) => {
   // Sends request...
   const user = await request(app).get("/fetchAllSprints");
   expect(user).toBeTruthy();
+  done();
+});
+
+afterAll(async (done) => {
+  await mongoose.disconnect();
+  done();
 });
