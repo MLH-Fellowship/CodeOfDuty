0bc006d812c34b2b1a9c6f5c993d2e94be75b18e
Chau Vu <chv004@ucsd.edu>
2020-11-19 16:09:45 -0800

    Added eslint, prettier, and husky for automated linting

--- codeofduty-frontend/src/App.js
+++ codeofduty-frontend/src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./routes/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/" component={Home} />
       </Router>
     </div>
   );
