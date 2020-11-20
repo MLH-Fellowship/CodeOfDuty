26b45a76930fff78f4c09b04b00a29cdb72dc3ed
Ajwad Shaikh <37571185+ajwad-shaikh@users.noreply.github.com>
2020-11-21 00:36:55 +0530

    Homepage UI & Sprints - Global and User (#30)
    * Add Material UI Components
    
    * Add Global and User Sprint Tables
    
    * Sprint and Repo Links Embedded
    
    * Add Icon Pack
    
    * Add all routes and small fixes

--- codeofduty-frontend/src/App.js
+++ codeofduty-frontend/src/App.js
@@ -2,12 +2,22 @@ import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./routes/Home";
+import SprintLayout from "./routes/SprintLayout";
+import RepoLayout from "./routes/RepoLayout";
+import CreateLayout from "./routes/CreateLayout";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Route exact path="/" component={Home} />
+        <Route
+          exact
+          path="/repo/:owner/:repo/:sprintPermId"
+          component={SprintLayout}
+        />
+        <Route exact path="/repo/:owner/:repo" component={RepoLayout} />
+        <Route exact path="/createSprint" component={CreateLayout} />
       </Router>
     </div>
   );
---
+++ codeofduty-frontend/src/routes/RepoLayout.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { withStyles } from "@material-ui/core";
+
+const style = {};
+
+class RepoLayout extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setState({});
+  }
+
+  render() {
+    return <div>RepoLayout</div>;
+  }
+}
+
+export default withStyles(style)(RepoLayout);
