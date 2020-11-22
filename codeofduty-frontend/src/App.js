5250b3c5f3eef455aa594e3d2e5998b667c0f99b
Ajwad Shaikh <37571185+ajwad-shaikh@users.noreply.github.com>
2020-11-23 01:09:39 +0530

    Sprint View - Backend + Frontend (#38)
    * Home Component
    
    * Add Header to other routes
    
    * Repo & Sprints fetch endpoint w/tests
    
    * Load data on the frontend
    
    * Added Healthbar and Leaderboard Components
    
    * Added Taskboard and BOSS gifs
    
    * Make monster smaller on sprint completion
    
    * Fix failing test due to schema change
    
    * Add task title to schema
    
    * Remove Unnecessary CreateLayout.js
    
    * Sprint view updates
    
    * Remove unnecessary comments

--- codeofduty-frontend/src/App.js
+++ codeofduty-frontend/src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
 import Home from "./routes/Home";
 import SprintLayout from "./routes/SprintLayout";
 import RepoLayout from "./routes/RepoLayout";
-import CreateLayout from "./routes/CreateLayout";
 
 function App() {
   return (
     <div className="App">
+      <CssBaseline />
       <Router>
         <Route exact path="/" component={Home} />
         <Route
@@ -17,7 +18,6 @@ function App() {
           component={SprintLayout}
         />
         <Route exact path="/repo/:owner/:repo" component={RepoLayout} />
-        <Route exact path="/createSprint" component={CreateLayout} />
       </Router>
     </div>
   );
