b70a6bc93fd2dbf67494522235082f799e166583
Ajwad Shaikh <37571185+ajwad-shaikh@users.noreply.github.com>
2020-11-24 05:44:20 +0530

    Repo View and Sprint View Merged (#48)
    * Add repo leaderboard, move monster
    
    * Manage Repo Page w/o sprint perm id
    
    * redirect on sprint change

---
+++ codeofduty-frontend/src/components/RepoLeaderBoard.js
@@ -0,0 +1,62 @@
+import {
+  withStyles,
+  TableContainer,
+  TableHead,
+  TableBody,
+  TableCell,
+  Table,
+  Paper,
+  TableRow,
+} from "@material-ui/core";
+import EmojiEventIcon from "@material-ui/icons/EmojiEvents";
+import React from "react";
+
+const style = {
+  table: {
+    marginTop: 25,
+    minWidth: 200,
+  },
+};
+
+class RepoLeaderboard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { participantData } = this.props;
+    return (
+      <TableContainer component={Paper} className={classes.table}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Participant</TableCell>
+              <TableCell align="right">Points Claimed</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {participantData &&
+              participantData.map((participant, index) => (
+                // eslint-disable-next-line no-underscore-dangle
+                <TableRow key={participant.user}>
+                  <TableCell component="th" scope="row">
+                    <a href={`https://github.com/${participant.user}`}>
+                      {participant.user}
+                    </a>
+                    {index === 0 && <EmojiEventIcon />}
+                  </TableCell>
+                  <TableCell align="right">
+                    {participant.points_claimed}
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
+  }
+}
+
+export default withStyles(style)(RepoLeaderboard);
--- codeofduty-frontend/src/routes/RepoLayout.js
+++ codeofduty-frontend/src/routes/RepoLayout.js
@@ -1,30 +1,54 @@
 import React from "react";
 import axios from "axios";
-import { withStyles } from "@material-ui/core";
-import Header from "../components/Header";
+import {
+  FormControl,
+  ListSubheader,
+  MenuItem,
+  Select,
+  Typography,
+  withStyles,
+} from "@material-ui/core";
+import RepoLeaderBoard from "../components/RepoLeaderBoard";
 
 const style = () => ({
   root: {
     height: "95%",
     width: "95%",
   },
+  sub: {
+    display: "inline-block",
+    margin: "20px auto auto auto",
+  },
+  formControl: {
+    display: "inline-block",
+    margin: "20px auto auto auto",
+    width: "80%",
+  },
+  select: {
+    width: "80%",
+    textAlign: "left",
+  },
 });
 
 class RepoLayout extends React.Component {
   constructor(props) {
     super(props);
-    // eslint-disable-next-line react/destructuring-assignment
-    const { owner, repo } = this.props.match.params;
+    const { owner, repo } = this.props;
     this.state = {
       owner,
       repo,
-      data: {},
+      data: null,
     };
   }
 
   componentDidMount() {
     this.fetchRepoData();
   }
+
+  handleSelectChanged = (event) => {
+    const { owner, repo, redirect } = this.props;
+    redirect(`repo/${owner}/${repo}/${event.target.value}`);
+  };
 
   async fetchRepoData() {
     const { owner, repo } = this.state;
@@ -36,13 +60,49 @@ class RepoLayout extends React.Component {
   }
 
   render() {
-    const { classes } = this.props;
-    // eslint-disable-next-line no-unused-vars
+    const { classes, sprintId } = this.props;
     const { owner, repo, data } = this.state;
     return (
       <div className={classes.root}>
-        <Header />
-        {`${owner}/${repo}`}
+        <Typography variant="h5" align="center">
+          <a href={`https://github.com/${owner}/${repo}`}>
+            {`${owner}/${repo}`}
+          </a>
+        </Typography>
+        {data && (
+          <div>
+            <Typography className={classes.sub} variant="h6" align="center">
+              Sprint
+            </Typography>
+            <FormControl className={classes.formControl}>
+              <Select
+                className={classes.select}
+                defaultValue={sprintId || data.active_sprints[0].sprint_perm_id}
+                id="grouped-select"
+                onChange={this.handleSelectChanged}
+              >
+                <ListSubheader>Active Sprints</ListSubheader>
+                {data.active_sprints.map((sprint) => (
+                  // eslint-disable-next-line no-underscore-dangle
+                  <MenuItem key={sprint._id} value={sprint.sprint_perm_id}>
+                    {sprint.name}
+                  </MenuItem>
+                ))}
+                <ListSubheader>Past Sprints</ListSubheader>
+                {data.past_sprints.map((sprint) => (
+                  // eslint-disable-next-line no-underscore-dangle
+                  <MenuItem key={sprint._id} value={sprint.sprint_perm_id}>
+                    {sprint.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+            <Typography className={classes.sub} variant="h6" align="center">
+              Repo Leaderboard
+            </Typography>
+            <RepoLeaderBoard participantData={data.contributors} />
+          </div>
+        )}
       </div>
     );
   }
